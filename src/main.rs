1a6471a1af394bbe5efcba0e9fabb37f5b2fa86a
Billy J Rhoades II <me@brod.es>
2023-12-04 16:46:07 -0800

    2023/04: add impl

--- src/main.rs
+++ src/main.rs
@@ -55,6 +55,7 @@ fn main() -> Result<()> {
         (2023, 1) => year_2023::day_01::run(input),
         (2023, 2) => year_2023::day_02::run(input),
         (2023, 3) => year_2023::day_03::run(input),
+        (2023, 4) => year_2023::day_04::run(input),
         (2023, _) | (2022, _) => bail!("unknown problem number {}", opt.problem),
         (_, _) => bail!("unkown year {}", opt.year),
     }
