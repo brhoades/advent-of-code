a346052cd2139f4b7c406e60634f452b6a2f646e
Billy J Rhoades II <me@brod.es>
2025-12-20 12:58:01 -0800

    aoc25: init go

---
+++ src/go/cmd/aoc/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"brod.es/aoc/internal/twentyfive"
+	"github.com/urfave/cli/v3"
+)
+
+func main() {
+	var year, day, part int
+	cmd := &cli.Command{
+		UseShortOptionHandling: true,
+		Commands: []*cli.Command{
+			{
+				Name:  "run",
+				Usage: "Run a specific year day and part of AoC",
+				Arguments: []cli.Argument{
+					&cli.IntArg{Name: "year", Destination: &year},
+					&cli.IntArg{Name: "day", Destination: &day},
+					&cli.IntArg{Name: "part", Destination: &part},
+				},
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					if year != 25 {
+						return fmt.Errorf("unknown year: %d", year)
+					}
+
+					switch day {
+					case 1:
+						return twentyfive.One(part)
+					}
+
+					return fmt.Errorf("unknown day: %d", day)
+				},
+			},
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ src/go/internal/twentyfive/one.go
@@ -0,0 +1,7 @@
+package twentyfive
+
+import "fmt"
+
+func One(part int) error {
+	return fmt.Errorf("unimpl'd")
+}
