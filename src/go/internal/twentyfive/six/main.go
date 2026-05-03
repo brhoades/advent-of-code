1f548eb189e64757ce4eeb7797ca09d5d6f542e7
Billy J Rhoades II <me@brod.es>
2026-05-03 13:20:56 -0700

    25.6 add pt2

--- src/go/internal/twentyfive/six/main.go
+++ src/go/internal/twentyfive/six/main.go
@@ -31,10 +31,15 @@ func Main(path string) error {
 		return err
 	}
 
-	fmt.Printf("%+v\n", columns)
+	fmt.Printf("part 1 parse: %+v\n", columns)
+	fmt.Printf("part 1 result: %d\n", solve(columns))
 
-	total := solve(columns)
-	fmt.Printf("part 1 result: %d\n", total)
+	if columns, err = parsePart2(path); err != nil {
+		return err
+	}
+
+	fmt.Printf("part 2 parse: %+v\n", columns)
+	fmt.Printf("part 2 result: %d\n", solve(columns))
 
 	return nil
 }
@@ -94,6 +99,68 @@ func parse(path string) ([]column, error) {
 	}
 
 	return ret, nil
+}
+
+// Parses a file which contains whitespace-separated numbers on lines. The final line should be an operation.
+// Numbers are read top-to-bottom.
+func parsePart2(path string) ([]column, error) {
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read %s: %w", path, err)
+	}
+
+	// for my poor brain, it's easiest to parse into a grid of [][]string then work
+	// on those.
+	rows := slices.Collect(strings.Lines(string(bytes)))
+	// grid[x][y] => num
+	xyGrid := make([][]string, len(rows))
+	for row_num, row := range rows {
+		xyGrid[row_num] = strings.Split(row, "")
+	}
+
+	grid := rotate(xyGrid)
+
+	first := true
+	columns := []column{}
+	for i, splitRow := range grid {
+		row := strings.TrimSpace(strings.Join(splitRow, ""))
+		if row == "" {
+			first = true
+			continue
+		} else if first {
+			first = false
+			columns = append(columns, column{
+				// last symbol is op for first lines
+				op: row[len(row)-1:],
+			})
+			row = strings.TrimSpace(row[:len(row)-1])
+		}
+
+		num, err := strconv.Atoi(row)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse '%s' as number on row %d: %w", row, i, err)
+		}
+
+		columns[len(columns)-1].nums = append(columns[len(columns)-1].nums, num)
+
+	}
+
+	return columns, nil
+}
+
+// Rotates a 2d grid clockwise. Assumes the grid is dense and all
+// rows have the same length.
+//
+// Could do this in place but... please god no.
+func rotate[T any](grid [][]T) [][]T {
+	ret := make([][]T, len(grid[0]))
+	for row_num, _ := range grid {
+		for col_num, cell := range grid[row_num] {
+			ret[col_num] = append(ret[col_num], cell)
+		}
+	}
+
+	return ret
 }
 
 func solve(cols []column) (sum int) {
